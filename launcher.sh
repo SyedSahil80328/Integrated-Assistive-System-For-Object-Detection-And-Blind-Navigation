a57cf373ac36ca57bd273dc032a57cc5a73fa2b4
Ashwanth-R <96193365+Ashwanth-R@users.noreply.github.com>
2024-02-24 12:02:50 +0530

    Commiting Launcher script

---
+++ launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Loading..."
+sleep 5
+echo "Changing Directory"
+cd /home/mass/Desktop/Obj-Detect-and-Depth/object-detect #Replace it with your actual path.
+echo "Setting XDG_RUNTIME_DIR"
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+echo "Setting actual permissions for runtime directory"
+sudo chmod 0700 /run/user/1000
+echo "Running Python script"
+python3 objectdetectorprototype.py
+cd
