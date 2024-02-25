72458c6b66ce295bdaf3baa8baaa49610321fd9a
Mugundh <mugundhjb@gmail.com>
2024-02-25 19:40:44 +0530

    Update object detection, voice output

--- constants.py
+++ constants.py
@@ -31,6 +31,6 @@ classNames = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "trai
               "elephant", "bear", "zebra", "giraffe", "hat", "backpack", "umbrella", "shoe", "eye glasses", "handbag", "tie", "suitcase", 
               "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", 
               "bottle", "plate", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", 
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "mirror", "dining table", "window", 
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "plant", "bed", "mirror", "dining table", "window", 
               "desk", "toilet", "door", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", 
               "sink", "refrigerator", "blender", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush", "hair brush"]
